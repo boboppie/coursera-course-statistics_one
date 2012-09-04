54cf4feecaec2a7a4f595de91795a5eb2f2cab8f
Fengyuan Hu <fengyuan.huu@gmail.com>
2012-09-04 13:24:32 +0200

    code style

--- week1/lecture3/wine_tasting.R
+++ week1/lecture3/wine_tasting.R
@@ -6,13 +6,13 @@
 library(psych)
 
 # Read the table into a dataframe called ratings
-ratings <- read.table("supplemental-stats1_ex01.txt", header=T)
+ratings <- read.table("supplemental-stats1_ex01.txt", header = T)
 # or do:
 ## supplemental.stats1_ex01 <- read.delim("~/git/coursera-course-statistics_one/week1/lecture3/supplemental-stats1_ex01.txt")
 ## View(supplemental.stats1_ex01)
 
 # Explore the contents of the dataframe
-View(ratings) # display the data
+View(ratings)  # display the data
 class(ratings) # the type of object, returns "data.frame"
 names(ratings) # list the name of variables in ratings, returns the headers
 
@@ -20,11 +20,11 @@ names(ratings) # list the name of variables in ratings, returns the headers
 # e.g. hist(ratings$RedTruck)
 
 # Plot four histograms on one page
-layout(matrix(c(1,2,3,4), 2, 2, byrow=TRUE)) # create 2x2 matrix for layout
-hist(ratings$WoopWoop, xlab="Rating")
-hist(ratings$RedTruck, xlab="Rating")
-hist(ratings$HobNob, xlab="Rating")
-hist(ratings$FourPlay, xlab="Rating")
+layout(matrix(c(1,2,3,4), 2, 2, byrow = TRUE)) # create 2x2 matrix for layout
+hist(ratings$WoopWoop, xlab = "Rating")
+hist(ratings$RedTruck, xlab = "Rating")
+hist(ratings$HobNob, xlab = "Rating")
+hist(ratings$FourPlay, xlab = "Rating")
 
 # Learn more about functions
 ## help(hist)
