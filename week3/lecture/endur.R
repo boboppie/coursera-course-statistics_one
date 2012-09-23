a8c071b8110b2b9fce8a196aa0d0fa10274982f5
Fengyuan Hu <fengyuan.huu@gmail.com>
2012-09-23 23:00:18 +0100

    script changed

--- week3/lecture/endur.R
+++ week3/lecture/endur.R
@@ -21,3 +21,22 @@ summary(model2)
 
 model3 = lm(endur$endurance ~ endur$age + endur$activeyears)
 summary(model3)
+
+# Regression analysis (standardized)
+model1.z = lm(scale(endur$endurance) ~ scale(endur$age))
+summary(model1.z)
+
+model2.z = lm(scale(endur$endurance) ~ scale(endur$activeyears))
+summary(model2.z)
+
+model3.z = lm(scale(endur$endurance) ~ scale(endur$age) + scale(endur$activeyears))
+summary(model3.z)
+
+# Model comparisons
+comp1 = anova(model1.z, model3.z)
+comp1
+comp2 = anova(model2.z, model3.z)
+comp2
+
+drops <- c("pid")
+cor(endur[,!(names(endur) %in% drops)])
